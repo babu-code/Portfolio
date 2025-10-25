848d42443ad67eccce06fa41fe6a50af2220332e
Kiplimo Antony <tonnieblair12@gmail.com>
2025-10-26 01:02:34 +0300

    Updated link for stocks-dashboard

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,9 +5,11 @@ const Project = ({imageSrc, title, description ,stack}) => {
   return (
 <div className="project-card card p-6 rounded-lg cursor-pointer" data-project="1">
     <div className="h-48 mb-4 rounded-lg overflow-hidden">
-        <img src= {imageSrc}
+        <a href="https://stock-dashboard-kappa-ten.vercel.app/">
+          <img src= {imageSrc}
              alt="E-commerce Dashboard" 
              className="w-full h-full object-cover" />
+        </a>
     </div>
     <h3 className="text-xl font-bold mb-2">{title}</h3>
     <p className="text-gray-300 mb-4">{description}</p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,37 +9,29 @@ const Projects = () => {
         
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {/* <!-- Project 1 --> */}
-            <a href="#">
-              <Project imageSrc={"https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"}
-              title={"Ecommerce Website "}
-                stack={[{label:"React", bg:"bg-indigo-500"}, {label:"Flask", bg:"bg-green-900"}, {label:"Postgres", bg:"bg-indigo-900"}]}
-              description={"A comprehensive e-commerce website complete with payment systems and analytics dashboard for online stores with real-time data visualization."}/>
-              
+            <Project imageSrc={"https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"}
+             title={"Ecommerce Website "}
+              stack={[{label:"React", bg:"bg-indigo-500"}, {label:"Flask", bg:"bg-green-900"}, {label:"Postgres", bg:"bg-indigo-900"}]}
+             description={"A comprehensive e-commerce website complete with payment systems and analytics dashboard for online stores with real-time data visualization."}/>
+            
 
-            </a>
-            <a href="https://stock-dashboard-kappa-ten.vercel.app/">
-              <Project imageSrc={"https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" }
-              title ={"Finance Dashboard"}
-              stack = {[{label: "JavaScript", bg:"bg-yellow-600"}, {label:"React", bg:"bg-indigo-500"}, {label:"Flask", bg:"bg-green-900"} ]}
-              description = {"An AI-powered tool that generates marketing content based on user prompts."} 
-              />
-            </a>
+            <Project imageSrc={"https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" }
+            title ={"Finance Dashboard"}
+            stack = {[{label: "JavaScript", bg:"bg-yellow-600"}, {label:"React", bg:"bg-indigo-500"}, {label:"Flask", bg:"bg-green-900"} ]}
+            description = {"An AI-powered tool that generates marketing content based on user prompts."} 
+            />
 
-            <a href="#">
             <Project imageSrc={"https://images.unsplash.com/photo-1681398836231-d0b89bd571d6?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
             title={"Jobs Website"}
             stack={[{label:"React", bg:"bg-indigo-500"}, {label:"Flask", bg:"bg-green-900"}]}
             description={"A Platform that allows employers to manage job vacancies "}
             />
-            </a>
             
-            <a href="#">
-              <Project imageSrc={"https://plus.unsplash.com/premium_photo-1728412897842-06f0fc4c2ec6?q=80&w=945&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
-              title = {"Recipe Sharing Platform"}
-              stack = {[{label:"React", bg:"bg-indigo-500"}, {label:"Flask", bg:"bg-green-900"}]}
-              description={"A recipe sharing platform that enables users to create,update, delete and share recipes"}
-              />
-            </a>
+            <Project imageSrc={"https://plus.unsplash.com/premium_photo-1728412897842-06f0fc4c2ec6?q=80&w=945&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+            title = {"Recipe Sharing Platform"}
+            stack = {[{label:"React", bg:"bg-indigo-500"}, {label:"Flask", bg:"bg-green-900"}]}
+            description={"A recipe sharing platform that enables users to create,update, delete and share recipes"}
+            />
             
             
       </section>
